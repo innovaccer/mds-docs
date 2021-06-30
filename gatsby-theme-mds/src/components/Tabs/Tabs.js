52dec6ecd89ea6ef121f47ee99de7a363a886cba
SachinInnov <sachin.thakur@innovaccer.com>
2021-06-30 09:58:16 +0530

    added tab component
    added tabs in contriutions
    
    minor change

---
+++ gatsby-theme-mds/src/components/Tabs/Tabs.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { TabsWrapper, Tab, Text } from '@innovaccer/design-system';
+
+// import NavContext from '../../util/context/NavContext';
+
+const Tabs = (props) => {
+  const [activeIndex, setActive] = useState(0);
+
+  const { tabData } = props;
+
+  const onTabChangeHandler = (tabIndex) => {
+    setActive(tabIndex);
+  };
+
+  return (
+    <TabsWrapper active={activeIndex} onTabChange={onTabChangeHandler}>
+      {tabData &&
+        tabData.length &&
+        tabData.map((tab, index) => {
+          return (
+            <Tab
+              label={
+                <>
+                  <Text
+                    tabIndex={index + 1}
+                    appearance={activeIndex !== index ? 'subtle' : 'link'}
+                  >
+                    {tab.tabName}
+                  </Text>
+                </>
+              }
+            >
+              <div>{tab.data}</div>
+            </Tab>
+          );
+        })}
+    </TabsWrapper>
+  );
+};
+
+export default Tabs;
