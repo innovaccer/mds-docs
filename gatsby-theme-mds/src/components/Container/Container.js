72e76055e684406ca80faf37fd64ef7ec35ec3aa
SachinInnov <sachin.thakur@innovaccer.com>
2021-08-11 10:31:44 +0530

    created data extraction and integration for components
    removed redundant code
    
    added storybook data
    
    removed redundant files

--- gatsby-theme-mds/src/components/Container/Container.js
+++ gatsby-theme-mds/src/components/Container/Container.js
@@ -4,8 +4,8 @@ import { Card, Heading } from '@innovaccer/design-system';
 const Container = ({ children, pageTitle }) => {
   return (
     <>
-      <Heading size="xl" className="my-5">{pageTitle}</Heading>
-      {children}
+      <Heading className="my-5">{pageTitle}</Heading>
+      <Card className="p-6">{children}</Card>
     </>
   );
 };
