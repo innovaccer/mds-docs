fd94222ec7f2d7fffbb37b3a112068b3ac2ae8e0
Satyam Yadav <satyamyadav3@gmail.com>
2021-08-25 00:00:04 +0530

    feat: adds code block config and styling

--- gatsby-theme-mds/src/components/Layout.js
+++ gatsby-theme-mds/src/components/Layout.js
@@ -2,16 +2,30 @@
 import React, { useLayoutEffect } from 'react';
 import "@fontsource/nunito-sans";
 import "@innovaccer/design-system/css";
-import { Row, Column } from '@innovaccer/design-system';
+import { Row, Column, Card, Button } from '@innovaccer/design-system';
 import LeftNav from './LeftNav';
 import TableOfContent from './TableOfContent/TableOfContent';
 import Header from './Header';
 import Container from './Container';
 import ComponentsContainer from './Container/ComponentsContainer';
 import { MDXProvider } from "@mdx-js/react";
-import * as DSComponents from '@innovaccer/design-system';
+import * as MDSComponents from '@innovaccer/design-system';
 import Meta from './Meta';
 import '../css/style.css';
+
+
+const Code = ({ children, ...rest }) => {
+  return (
+    <>
+      <div {...rest}>
+        {children}
+      </div>
+      <Button icon="copy" />
+    </>
+  )
+};
+
+const DSComponents = { ...MDSComponents, code: Code }
 
 const leftMenuList = [
   {
